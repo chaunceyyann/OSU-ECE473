de9dc8e940a44669dbba116804daf22ff9e89bb9
cyanjin <chaunceyyann@gmail.com>
2014-10-23 00:55:05 -0700

    New Changes

--- lab2.c
+++ lab2.c
@@ -10,30 +10,14 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define TRUE 1
-#define FALSE 0
-#define INPUTP PINA
 //#define F_CPU 16000000 // cpu speed in hertz 
 #define MAX_CHECKS 12 // # checks before a switch is debounced
+#define BASE 10  // the base of the clock should be working 
+
 uint8_t debounced_state = 0; // Debounced state of the switches
 uint8_t state[MAX_CHECKS]; // Array that maintains bounce status
-uint8_t index = 0; // Pointer into State
-// Service routine called by a timer interrupt
-
-void DebounceSwitch(){
-	uint8_t i,j;
-	state[index++]=0xff - PINA;
-    j=0xff;
-    for(i=0; i<MAX_CHECKS-1;i++)j=j & state[i];
-	//		if(j > 0) PORTF = 0xff;
-	//		else PORTF = 0x00;
-	debounced_state= j;
-    if(index >= MAX_CHECKS)index=0;
-}
-
-
+uint8_t id = 0; // Pointer into State
 //decimal to 7-segment LED display encodings, logic "0" turns on segment
-//uint8_t dec_to_7seg[12] 
 int dec_to_7seg[18] = {0b11000000, 0b11111001, 0b10100100, 0b10110000, // 0 1 2 3
 					   0b10011001, 0b10010010, 0b10000010, 0b11111000, // 4 5 6 7
 					   0b10000000, 0b10011000, 0b10001000, 0b10000011, // 8 9 A B
@@ -52,19 +36,16 @@ int segment_data[5] = {0b11000000, 0xff, 0xff, 0xff, 0xff}; // turn off led not 
 //Adapted to check all buttons from Ganssel's "Guide to Debouncing"            
 //Expects active low pushbuttons on PINA port.  Debounce time is determined by 
 //external loop delay times 12. 
-//
-uint8_t chk_buttons(uint8_t button) {
-	static uint16_t state = 0; //holds present state
-	state = (state << 1) | (! bit_is_clear(INPUTP, button)) | 0xE000;
-	if (state == 0xF000) return 1;
-	return 0;
+void DebounceSwitch(){
+	uint8_t i,j;
+	state[id++]=0xff - PINA;
+    j=0xff;
+    for(i=0; i<MAX_CHECKS-1;i++)j=j & state[i];
+	debounced_state = j;
+    if(id >= MAX_CHECKS)id=0;
 }
-uint8_t chk_button(uint8_t button) {
-	static uint16_t state = 0; //holds present state
-	state = (state << 1) | (! bit_is_clear(INPUTP, button)) | 0xE000;
-	if (state == 0xF000) return 1;
-	return 0;
-}
+
+
 //**********************************************************************************
 //                                   segment_sum                                    
 //takes a 16-bit binary input value and places the appropriate equivalent 4 digit 
@@ -72,25 +53,34 @@ uint8_t chk_button(uint8_t button) {
 //array is loaded at exit as:  |digit3|digit2|colon|digit1|digit0|
 //**********************************************************************************
 void segsum(uint16_t sum) {
-	uint16_t dgt; 
-	int u = 4, o;
-	//int l = 0;
-
-  	//determine how many digits there are 
-	//while (!(sum & 0x8000)){
-	//	sum << 1;
-	//	l++;
-	//}
-	//l = l/4; // l repersent how many empty digit in front
+	int u = 4;
 	
   	//break up decimal sum into 4 digit-segments
-	for (o = 0; o < 4; o++){
-		dgt = (sum >> (o * 4)) & 0x000f;
-		segment_data[o] = dec_to_7seg[dgt];
+	switch (BASE) {
+	case 10:
+		//breakDgt(sum,10);
+		segment_data[0] = dec_to_7seg[sum%10];
+		segment_data[1] = dec_to_7seg[sum/10%10];
+		segment_data[2] = dec_to_7seg[sum/100%10];
+		segment_data[3] = dec_to_7seg[sum/1000%10];
+		break;
+	case 16:
+		breakDgt(sum,4,0x000f);
+		break;
+	case 8:
+		breakDgt(sum,3,0x0007);
+		break;
+	case 2:
+		breakDgt(sum,1,0x0001);
+		break;
 	}
   	//blank out leading zero digits 
 	while (segment_data[u] == dec_to_7seg[0] || segment_data[u] == 0xff) {
 		segment_data[u--] = 0xff;
+		if ( u == -1) {
+			segment_data[0] = dec_to_7seg[0];
+			break;
+		}
 	}
   	//now move data to right place for misplaced colon position
 	segment_data[4] = segment_data[3];
@@ -98,6 +88,14 @@ void segsum(uint16_t sum) {
 	segment_data[2] = 0xff; //dec_to_7seg[17];
 }
 
+void breakDgt(uint16_t sum, int base_count, uint16_t check_bit) { 
+	uint16_t dgt;
+	int o;
+	for (o = 0; o < 4; o++){
+		dgt = (sum >> (o * base_count)) & check_bit;
+		segment_data[o] = dec_to_7seg[dgt];
+	}
+}
 //***********************************************************************************
 // Demostration and debugging functions
 //
@@ -150,25 +148,23 @@ while(1){
 
 
   	//now check each button and increment the count as needed
-	//j = counter%8;
-	//for (j = 0; j < 8; j++){
-		//for (i = 0; i < 12;i++){
-			DebounceSwitch();
-			if (debounced_state){
-				if(release == 0){
-				release = 1;
-				count += debounced_state; 
-  				//bound the count to 0 - 1023
-				count = count%65536;
-  				//break up the disp_value to 4, BCD digits in the array: call (segsum)
-				segsum(count);
-				debounced_state = 0;	
-			}
-}
-			else release = 0;
-			//_delay_ms(1);
-	//}
+	DebounceSwitch();
+	if (debounced_state){ // check if any of the buttons is pressed
+		if(release == 0){
+			release = 1;
+			count += debounced_state; 
+  			//bound the count to 0 - 1023
+			count = countaver
+				1024;
+  			//break up the disp_value to 4, BCD digits in the array: call (segsum)
+			segsum(count);
+			debounced_state = 0;	
+		}
+	}
+	else release = 0;
+	//_delay_ms(1);
 	
+
 	//make PORTA an output
 	DDRA = 0xff;
 	//bound a counter (0-4) to keep track of digit to display 
@@ -180,6 +176,7 @@ while(1){
   		//fix for the last digit over bright issue
 		if (counter != 4)
 			_delay_ms(2);
+
 	}
   }//while
 }//main
